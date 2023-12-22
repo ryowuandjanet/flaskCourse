b00f99e1cae4d671a3c7472150d970de08d210a1
ryowuandjanet <ryowu0329@gmail.com>
2023-12-22 11:07:04 +0800

    12 - User Authentication Part 2

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, 'db.sqlite')
@@ -10,5 +11,6 @@ app.config['SECRET_KEY'] = 'be05e2df0b87120739df2075'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
 
