b95591e05682572bdce44822761d6e982914f4d2
ryowuandjanet <ryowu0329@gmail.com>
2023-12-22 09:55:53 +0800

    09 - Flask Validations

--- market/models.py
+++ market/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
-    
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -15,6 +15,5 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
-
     def __repr__(self):
         return f'Item {self.name}'
