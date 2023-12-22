9280bbb4f2db60e7c2cd821ae6e86f616c0f12fa
ryowuandjanet <ryowu0329@gmail.com>
2023-12-22 09:03:15 +0800

    08 - Flask Forms

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, 'db.sqlite')
+app.config['SECRET_KEY'] = 'be05e2df0b87120739df2075'
 db = SQLAlchemy(app)
 
 from market import routes
